c9b04a70032ebea3aea813218426da827ed736c7
JstarPlatinum <114332679+JstarPlatinum@users.noreply.github.com>
2026-05-10 11:10:20 +0800

    modified:   classguard/main/main.c

--- classguard/main/main.c
+++ classguard/main/main.c
@@ -1,20 +1,7 @@
-#include "app_data.h"
-#include "esp_err.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "sensor_service.h"
-#include "telemetry_upload.h"
+#include "mlx90640_data_test.h"
+
 
 void app_main(void)
 {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    cg_app_data_init();
-    ESP_ERROR_CHECK(cg_sensor_service_start());
-    ESP_ERROR_CHECK(cg_telemetry_upload_start());
+    mlx90640_data_test_start();
 }
